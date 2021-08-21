de2f2f6dbe71b07cac8e8d58ee3b505c4a6549cf
deGekata <y.zvonkov@yandex.ru>
2021-08-21 20:07:12 +0300

    fix code structure

--- equation/equation/Source.cpp
+++ equation/equation/Source.cpp
@@ -3,20 +3,22 @@
 
 
 int getVar(double* a) {
-    char ch;
+    char ch = getchar();
     short int sign = 1;
-    ch = getchar();
     if (ch == '-') {
         sign = -1;
         ch = getchar();
     }
+
     while (ch <= '9' && ch >= '0') {
         (*a) = (*a) * 10.0 + (double)(ch - '0');
         ch = getchar();
     }
+
     if (!(ch == '.' || ch == '\n' || ch == ' ')) {
         return 0;
     }
+
     if (ch == '.') {
         double exp = 10.0;
         ch = getchar();
@@ -25,50 +27,69 @@ int getVar(double* a) {
             exp = exp * 10;
             ch = getchar();
         }
+
         if (!(ch == '\n' || ch == ' ')) {
             return 0;
         }
     }
+
     (*a) = (*a) * sign;
     return 1;
 }
 
 
 int getVars(double* a, double* b, double* c) {
-    return getVar(a) && getVar(b) && getVar(c);
+    int res = getVar(a) && getVar(b) && getVar(c);
+    if (*a == 0) {
+        return 0;
+    }
+    return res;
 }
 
 
-int main() {
-    double a = 0, b = 0, c = 0, d;
+int solve(double a, double b, double c) {
     int real = 1;
-    printf("Please, enter the coefficients.\nExample: For equation 2x^2 + 4x + 5 = 0\nWrite: 2 4 5.\n");
-    while (!getVars(&a, &b, &c)) {
-        printf("rewrite variables");
-    }
-    printf("%lf %lf %lf\n", a, b, c);
-
-    d = b * b - 4 * a * c;
-    if (d < 0) {
+    double D;
+    D = b * b - 4 * a * c;
+    if (D < 0) {
         real = 0;
         printf("Roots are complex\n");
-        d = -d;
-    } else {
+        D = -D;
+    }
+    else {
         printf("Roots are real\n");
     }
-    d = sqrt(d);
+    D = sqrt(D);
 
     if (real) {
-        double x1 = (-b - d) / (2 * a);
-        double x2 = (-b + d) / (2 * a);
+        double x1 = (-b - D) / (2 * a);
+        double x2 = (-b + D) / (2 * a);
         if (x1 != x2)
             printf("Roots: x1 = %lf and x2 = %lf", x1, x2);
         else
             printf("Root: x = %lf", x1);
-    } else {
+    }
+    else {
         printf("Complex Roots: x1 = %lf - %lf" "i and x2 = %lf + %lf" "i", -b / (2 * a), d / (2 * a), -b / (2 * a), d / (2 * a));
     }
 
+}
+
+void equation(double* a, double* b, double* c) {
+    printf("Please, enter the coefficients.\nExample: For equation 2x^2 + 4x + 5 = 0\nWrite: 2 4 5.\n");
+    while (!getVars(a, b, c)) {
+        if (a == 0) {
+            printf("first coefficient must not be equal to zero\n");
+        }
+        printf("rewrite variables");
+    }
+    printf("%lf %lf %lf\n", a, b, c);
+    solve(*a, *b, *c);
+}
+
+int main() {
+    double a = NAN, b = NAN, c = NAN, d = NAN;
+    equation(&a, &b, &c);
     getchar();
     return 0;
 }
