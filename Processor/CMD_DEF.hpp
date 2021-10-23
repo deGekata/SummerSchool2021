d2a233cdcbbb0a983da7ef394f8fc6f0d82ec59a
deGekata <y.zvonkov@yandex.ru>
2021-10-23 18:05:46 +0300

    test_args_type

--- Processor/CMD_DEF.hpp
+++ Processor/CMD_DEF.hpp
@@ -1,22 +1,30 @@
-DEF_CMD(HLT, 0, 1, ~empty & flags, { //emtpty 
+DEF_CMD(HLT, 0, 1, !(empty ^ flags), 
+{ 
     push(&stk, get_arg(code[ip + 1])); 
 })
 
-DEF_CMD(PUSH, 1, 1, ~(mem | reg | immediate_constant) & flags,
+DEF_CMD(PUSH, 1, 1, !((mem | reg | immediate_constant) ^ flags) || 
+                    !((mem | reg) ^ flags)                      || 
+                    !(reg ^ flags)                              || 
+                    !((mem | immediate_constant) ^ flags)       ||
+                    !((reg | immediate_constant) ^ flags)       ||
+                    !(immediate_constant ^ flags)               ||
+                    !(reg ^ flags)
 {  //immediate_contant | reg | mem
     push(&stk, get_arg(code[ip + 1])); 
 })
 
 
-DEF_CMD(POP, 2, 1, (~(mem | reg | immediate_constant) & flags) || 
-                      (~(mem | reg) & flags)                   || 
-                      (~reg & flags)                           || 
-                      (~(mem | immediate_constant) & flags)  ,
+DEF_CMD(POP, 2, 1, !((mem | reg | immediate_constant) ^ flags) || 
+                   !((mem | reg) ^ flags)                      || 
+                   !(reg ^ flags)                              || 
+                   !((mem | immediate_constant) ^ flags)       ||
+                   !(reg ^ flags),
 {
     pop(&stk, get_arg(code[ip + 1])); 
 })
 
-DEF_CMD(JMP, 3, 1, mark, { 
+DEF_CMD(JMP, 3, 1, !(mark ^ flags), { 
     pop(&stk, get_arg(code[ip + 1])); 
 })
 
